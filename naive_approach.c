7f52f42d246c45d6a35376d05f7707b611d8cb86
Shreyansh Singh <shreyansh.pettswood@yahoo.com>
2020-10-18 01:00:41 +0530

    Update formatting and braces structure

--- naive_approach.c
+++ naive_approach.c
@@ -2,8 +2,8 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-int main()
-{
+
+int main() {
 	char a[100];
 	char b[100];
 	char c1[100];
@@ -14,29 +14,23 @@ int main()
 	int m=strlen(a);
 	int n=strlen(b);
 	int l,max=0,c=0,i,j,k,d;
-	for(i=1;i<=m;++i)
-	{
-		for(j=0;j<=m-i;++j)
-		{
+	for(i=1;i<=m;++i) {
+		for(j=0;j<=m-i;++j) {
 			c=0;
 			d=j;
-			for(k=0;k<n;++k)
-			{
-				if(b[k]==a[d])
-				{
+			for(k=0;k<n;++k) {
+				if(b[k]==a[d]) {
 					d++;
 					c++;
 					if(c==i)						
 						break;
 				}
-				else
-				{
+				else {
 					c=0;
 					d=j;
 				}
 			}
-			if(c==i&&max<i)
-			{
+			if(c==i&&max<i) {
 				c1[0]='\0';
 				for(l=j;l<j+i;++l)
 					c1[l-j]=a[l];
